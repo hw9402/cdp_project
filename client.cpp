1db1ebb56b78080d2bfd40512160572ad42b374a
마현우 <mhw35689402@gmail.com>
2022-12-09 03:14:29 +0900

    ADD :: printMenu() 함수 추가

--- client.cpp
+++ client.cpp
@@ -2,6 +2,17 @@
 #include <queue>
 using namespace std;
 
+class Order {
+   public:
+    void printMenu() {
+        cout << "              MENU             \n";
+        cout << " ------------------------------\n";
+        cout << "|     abcd버거 | ₩1000 | 1     |\n";
+        cout << "|     efgh버거 | ₩2000 | 2     |\n";
+        cout << " ------------------------------\n";
+    }
+};
+
 int main() {
     cout << "Hello world\n";
     return 0;
