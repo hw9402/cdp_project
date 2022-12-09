43b06032071ccee5de795ae4f4a436a4972dacef
HeeSungLee <a73482006@gmail.com>
2022-12-09 12:15:47 +0900

    Update SortIngrediant.cpp

--- SortIngrediant.cpp
+++ SortIngrediant.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 #include <stack>
 using namespace std;
+class SortIngrediant{
+    public:
+        int Hstack(int cnt + 1, int k,int *ingr[]){
+            int bottom = 0;
+            int top = cnt-1;
+            if(k > cnt){ 
+                stack.push
+                return 0;
+            }
+            stack.push(ingr[k]);
+            return Hstack(cnt, k+1, int *ingr[]);
+        }
+};
 
+int main(){
+    
+}
