888296ef0113efabb0ea3109854f9d8b1895f4b9
HeeSungLee <a73482006@gmail.com>
2022-12-09 10:34:55 +0900

    First commit

--- client.cpp
+++ client.cpp
@@ -1,46 +1,8 @@
 #include <iostream>
-#include <queue>
-#define MAX_MENU_COUNT 11
+#include <stack>
 using namespace std;
+stack<int> stack;
 
-class Order {
-   public:
-    queue<int> qMenu;
-    queue<int> qCount;
-    int menuNumber[MAX_MENU_COUNT];
-    int menuCount[MAX_MENU_COUNT];
-    int count, i;
-    void printMenu() {
-        cout << "              MENU             \n";
-        cout << " ------------------------------\n";
-        cout << "|     abcd버거 | ₩1000 | 1     |\n";
-        cout << "|     efgh버거 | ₩2000 | 2     |\n";
-        cout << " ------------------------------\n";
-    }
-    void orderMenu() {
-        cout << "메뉴 종류의 수를 입력해주세요(최대 10개) : ";
-        cin >> count;
-        cout << "메뉴를 입력해주세요\n"
-             << "형식 : 1(메뉴 번호) 1(개수)\n";
-        for (i = 1; i <= count; i++) {
-            cin >> menuNumber[i] >> menuCount[i];
-            qMenu.push(menuNumber[i]);
-            qCount.push(menuCount[i]);
-        }
-    }
-    void printResult() {
-        for (i = 1; i <= qMenu.size(); i++) {
-            cout << qMenu.front() << " " << qCount.front() << "\n";
-            qMenu.pop();
-            qCount.pop();
-        }
-    }
-};
-
-int main() {
-    Order a;
-    a.printMenu();
-    a.orderMenu();
-    a.printResult();
-    return 0;
+int main(){
+  cout<< "하루를 세면 돌아오는 길 위 에서 먼걸 음을 그냥 가려고 해요";
 }
